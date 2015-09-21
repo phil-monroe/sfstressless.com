3489d7b01a6ebfea7c357b5bbcb308ba67e8e77c
Phil Monroe <phil@philmonroe.com>
2015-09-20 19:07:10 -0700

    get it going

---
+++ js/application.js
@@ -0,0 +1,21 @@
+$(function() {
+  $('#sign-up button, #services .flex-box').on("click", function(){
+    $(".modal-state").prop("checked", true).change();
+  });
+
+  $("#modal-1").on("change", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
+
+  $(".modal-fade-screen, .modal-close").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
+});
